e0382699cdf55360e1d014dae4c1ae63d873eaf0
mw1213 <33518737+mw1213@users.noreply.github.com>
2020-06-07 20:25:10 +0200

    c code for displaying currently working tasks

---
+++ parking.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <time.h>
+
+
+void getPlateNumberFromVideo(int data)
+{
+   printf ("Opening gate in\n");
+}
+
+void startPlatesRecognitionIn()
+{
+   printf ("Recognizing plates for getting in\n");
+}
+
+void startPlatesRecognitionOut()
+{
+   printf ("Recognizing plates for getting out\n");
+}
+
+void startGateClosingIn()
+{
+   printf ("Closing gate in\n");
+}
+void incrementAvailableSlots()
+{
+   printf ("Incresing taken slots\n");
+}
+
+void decrementAvailableSlots()
+{
+   printf ("Decresing taken slots\n");
+}
+
+void calculatePriceForCarWithPlate()
+{
+   printf ("Calculating price of parking\n");
+}
+
+void startGateOpeningOut()
+{
+   printf ("Opening exit gate\n");
+}
+
+void startGateClosingOut()
+{
+   printf ("Closing exit gate\n");
+}
+
